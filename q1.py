63949de8d969ff677af9a292cef10cd0c1506969
wsandbox <warrenssandbox@gmail.com>
2020-01-06 19:52:00 -0500

    adding codes to one file for submission

--- q1.py
+++ q1.py
@@ -1,4 +1,5 @@
 import requests
 
 #print(requests.__version__)
-print(requests.get('https://www.google.com'))
+#print(requests.get('https://www.google.com'))
+var - requests.get("https://raw.githubuser.com/wsandbox/404lab1
